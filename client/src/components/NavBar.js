0c65df8a5782ae76876e7d91539e1ecd41c8ffc5
Nika Gazzaev <princdakkar@gmail.com>
2021-08-13 22:23:27 +0300

    Страница администратора, добавление товаров

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,12 +6,12 @@ import Container from "react-bootstrap/Container";
 import { NavLink } from "react-router-dom";
 import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utils/consts";
 import { Button } from "react-bootstrap";
-import {observer} from "mobx-react-lite"
+import { observer } from "mobx-react-lite";
 import { useHistory } from "react-router-dom";
 
 const NavBar = observer(() => {
   const { user } = useContext(Context);
-  const history = useHistory()
+  const history = useHistory();
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
@@ -20,17 +20,34 @@ const NavBar = observer(() => {
         </NavLink>
         {user.isAuth ? (
           <Nav className="ml-auto" style={{ color: "white" }}>
-            <Button variant={"outline-light"} onClick={()=> history.push(ADMIN_ROUTE)}>Админ панель</Button>
-            <Button variant={"outline-light"} className='ms-2' onClick={()=> history.push(LOGIN_ROUTE)}>Выйти</Button>
+            <Button
+              variant={"outline-light"}
+              onClick={() => history.push(ADMIN_ROUTE)}
+            >
+              Админ панель
+            </Button>
+            <Button
+              variant={"outline-light"}
+              className="ms-2"
+              onClick={() => history.push(LOGIN_ROUTE)}
+            >
+              Выйти
+            </Button>
           </Nav>
         ) : (
           <Nav className="ml-auto" style={{ color: "white" }}>
-            <Button variant={"outline-light"} onClick={()=>user.setIsAuth(true)}>Авторизация</Button>
+            <Button
+              variant={"outline-light"}
+              onClick={() => user.setIsAuth(true)}
+            >
+              Авторизация
+            </Button>
           </Nav>
         )}
       </Container>
     </Navbar>
   );
 });
+
 
 export default NavBar;
